5cf9d8d7d580be794135cc650f47bcfdd779d276
Stephen Brooks <smfbrooks@gmail.com>
2020-04-22 04:55:17 +0000

    Correct event callback for unintentional disconnects

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os
+import sys
 
 from dotenv import load_dotenv
 
@@ -21,11 +22,27 @@ async def on_disconnect():
     os.remove(bot.yt_file)
 
 
+#@bot.event
+#async def on_ready():
+#    guilds = await bot.fetch_guilds().flatten()
+#    log.info(f'I have connected to {", ".join([g.name for g in guilds])}')
+
+
 @bot.event
 async def on_ready():
     guilds = await bot.fetch_guilds().flatten()
     log.info(f'I have connected to {", ".join([g.name for g in guilds])}')
+    try:
+        # in case the bot disconnected inadvertently, make sure the file still exists
+        with open(bot.yt_file) as f:
+            pass
+    except (OSError, TypeError):
+        if not bot.yt_url:
+            log.info('Setting the alarm for the first time.')
+        else:
+            log.error(f'Could not find {bot.yt_file}. Redownloading it.')
 
+        bot.yt_url, bot.yt_tile, bot.yt_file = utils.download_yt(bot.yt_url)
 
 try:
     bot.add_cogs(bot)
@@ -35,3 +52,4 @@ except (SystemExit, KeyboardInterrupt) as e:
     log.critical(f'Encountered {type(e)}: {e}. Stopping!')
     bot.loop.run_until_complete(bot.logout())
     bot.loop.close()
+    sys.exit(1)
